5cdbf463f2be1a063080b2d239e5b188666516ac
macduy <macduy@localhost>
2010-09-04 14:03:43 +0100

    Playwav

--- Hai/soundanalyze.py
+++ Hai/soundanalyze.py
@@ -1,23 +1,37 @@
 import numpy
 import pyaudio
 import analyse
+import matplotlib.pyplot as plt
 
 # Initialize PyAudio
 pyaud = pyaudio.PyAudio()
 
 # Open input stream, 16-bit mono at 44100 Hz
-# On my system, device 2 is a USB microphone, your number may differ.
 stream = pyaud.open(
-    format = pyaudio.paInt16,
-    channels = 1,
-    rate = 44100,
-    input_device_index = 2,
-    input = True)
+	format = pyaudio.paInt16,
+	channels = 1,
+	rate = 44100,
+	input_device_index = 1,
+	input = True)
 
-while True:
-    # Read raw microphone data
-    rawsamps = stream.read(1024)
-    # Convert raw data to NumPy array
-    samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
-    # Show the volume and pitch
-    print analyse.loudness(samps), analyse.musical_detect_pitch(samps)
+    
+vols = []
+pitches  = []
+i = 0
+while i < 100:
+	# Read raw microphone data
+	rawsamps = stream.read(1024)
+	# Convert raw data to NumPy array
+	samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
+	# Show the volume and pitch
+	
+	vols.append(analyse.loudness(samps))
+	pitches.append(analyse.musical_detect_pitch(samps) or 0.0)
+	
+	print vols[-1]
+	
+	i += 1
+	
+plt.plot(vols)
+plt.plot(pitches, "ro")
+plt.show()
