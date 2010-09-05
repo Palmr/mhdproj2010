685e5fd7643592b544d62edd57b9a5888c0e06e0
nick <nick@nick.com>
2010-09-05 13:24:13 +0100

    Added controller types, but commented out musicification for testing

---
+++ marker.py
@@ -0,0 +1,17 @@
+
+
+from collections import deque
+import itertools
+from SendMidi import ControllerManager
+
+class Marker():
+	def __init__(self, pName, pIcon, pMatchColour, pMatchThreshold, pPosition, pFilterSize):
+		self.Name = pName
+		self.Icon = pIcon
+		self.MatchColour = pMatchColour
+		self.MatchThreshold = pMatchThreshold
+		self.Position = pPosition
+		self.SmoothingList = deque(list(itertools.repeat((0, 0), pFilterSize)))
+		self.MidiController = ControllerManager().choose()
+		self.ControllerType = raw_input("Choose controller type [X, Y, XY, Button]: ").upper()
+		self.OnScreen = True
